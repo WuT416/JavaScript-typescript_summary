97d1df13631fbe8ea35805cc928c6935fc376e4f
wuting12 <wuting12@meituan.com>
2023-09-20 19:46:09 +0800

    完成方法二补充

---
+++ js垃圾回收与内存泄漏/closure-circle-right2.js
@@ -0,0 +1,37 @@
+const heapdump = require("heapdump");
+
+var path = require("path");
+var startMemory = path.join(
+  __dirname,
+  "/file/start-circle-right2.heapsnapshot"
+);
+var endMemory = path.join(__dirname, "/file/end-circle-right2.heapsnapshot");
+heapdump.writeSnapshot(startMemory); // 记录应用开始时的内存dump
+
+(function () {
+  let foo = null;
+  function outer() {
+    let bar = foo;
+    function unused() {
+      // 未使用到的函数
+      console.log(`bar is ${bar}`);
+    }
+
+    foo = {
+      // 给foo变量重新赋值
+      bigData: new Array(100000).join("this_is_a_big_data"), // 如果这个对象携带的数据非常大，将会造成非常大的内存泄漏
+      inner: function () {
+        console.log(`inner method run`);
+      },
+    };
+    bar = null; // 手动释放bar变量，解除bar变量对上一次foo对象的引用
+  }
+  for (let i = 0; i < 1000; i++) {
+    outer();
+  }
+})();
+
+global.gc();
+heapdump.writeSnapshot(endMemory); // 记录应用结束时的内存dump
+
+console.log("结束-right");
--- js垃圾回收与内存泄漏/closure-wrong.js
+++ js垃圾回收与内存泄漏/closure-wrong.js
@@ -1,26 +1,24 @@
-const heapdump = require('heapdump');
+const heapdump = require("heapdump");
 
 var path = require("path");
-var startMemory = path.join(__dirname, "/file/start-wrong.heapsnapshot")
-var endMemory = path.join(__dirname, "/file/end-wrong.heapsnapshot")
+var startMemory = path.join(__dirname, "/file/start-wrong.heapsnapshot");
+var endMemory = path.join(__dirname, "/file/end-wrong.heapsnapshot");
 
 heapdump.writeSnapshot(startMemory); // 记录应用开始时的内存dump
 
-
 var replaceThing = function () {
-    //为了方便观察内存情况（正常情况下一般是不会有这么长的数组的），new一个有一亿项元素的数组，这样数组本身会占用很大的内存
-    var originalThing = new Array(100000).join("this_is_a_big_data");
-    var outer = 'outer str';
-    console.log('new...');
-    return function () {
-      if (originalThing)
-        console.log(outer);
-    };
+  //为了方便观察内存情况（正常情况下一般是不会有这么长的数组的），new一个有一亿项元素的数组，这样数组本身会占用很大的内存
+  var originalThing = new Array(100000).join("this_is_a_big_data");
+  var outer = "outer str";
+  console.log("new...");
+  return function () {
+    if (originalThing) console.log(outer);
   };
+};
 //得到闭包函数
 var closureFn = replaceThing();
 //执行
 closureFn();
 global.gc();
 heapdump.writeSnapshot(endMemory); // 记录应用结束时的内存dump
-console.log('结束-wrong')
+console.log("结束-wrong");
